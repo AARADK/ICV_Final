78aa868a69d099a6bd4c47ee51a796d5ba1246e2
AARADK <aarjanadhikari.39@gmail.com>
2026-05-15 10:39:24 +0545

    seven

--- seven.m
+++ seven.m
@@ -4,16 +4,66 @@ addpath("C:\Users\acer\Desktop\Matlab Final\Coursework Image Video");
 %% --------------------------------------------------------------------
 
 vid = VideoReader("video1.avi");
-vidOutput = VideoWriter("output\video1.avi");
-open(vidOutput);
-
-
-
-%% --------------------------------------------------------------------
-
-if ~exist("output", "dir")
-    mkdir("output")    
+outputFolder = "output";
+if ~exist(outputFolder, "dir")
+    mkdir(outputFolder);
 end
 
+% vid.CurrentTime = (14-1)/7;
+% img = readFrame(vid);
+% imshow(img);
+% grayImg = rgb2gray(im2double(img));
 
-% imwrite(vid, fullfile("output", "video1.avi"))
+% img = imbilatfilt(img, 0.05, 20);
+% BW = edge(grayImg, "canny");
+% BW = bwareaopen(BW, 150);
+
+% overlay = img;
+% red = overlay(:,:,1);
+% green = overlay(:,:,2);
+% blue = overlay(:,:,3);
+
+% red(BW) = 255;      
+% green(BW) = 0;      
+% blue(BW) = 0;       
+
+% overlay = cat(3, red, green, blue);
+
+% figure;
+% subplot(1,2,1);
+% imshow(BW);
+% title('Canny Edge Mask');
+
+% subplot(1,2,2);
+% imshow(overlay);
+% title('Canny Overlay on Frame');
+
+
+vidOutput = VideoWriter(fullfile(outputFolder, "video1.avi"));
+vidOutput.FrameRate = vid.FrameRate;
+open(vidOutput);
+
+while hasFrame(vid)
+    frame = readFrame(vid);
+    grayFrame = rgb2gray(im2double(frame));
+
+    frame = imbilatfilt(frame, 0.05, 20);
+    
+    BW = edge(grayFrame, "canny");
+    BW = bwareaopen(BW, 150);
+
+    overlay = frame;
+    red = overlay(:,:,1);
+    green = overlay(:,:,2);
+    blue = overlay(:,:,3);
+
+    red(BW) = 255;      
+    green(BW) = 0;      
+    blue(BW) = 0;       
+
+    overlay = cat(3, red, green, blue);
+    
+    writeVideo(vidOutput, overlay);
+end
+
+close(vidOutput);
